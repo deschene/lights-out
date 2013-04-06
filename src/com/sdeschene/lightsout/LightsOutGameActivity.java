f7c876a9453cf033ea79d354f77a21a9bcb1cf5d
Scott Deschene <deschene.scott@gmail.com>
2013-04-06 19:51:09 -0400

    Changed homescreen back to normal. no more weird gestures!

--- src/com/sdeschene/lightsout/LightsOutGameActivity.java
+++ src/com/sdeschene/lightsout/LightsOutGameActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
--- src/com/sdeschene/lightsout/StartScreenActivity.java
+++ src/com/sdeschene/lightsout/StartScreenActivity.java
@@ -3,20 +3,14 @@ package com.sdeschene.lightsout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class StartScreenActivity extends Activity {
-
-	private GestureDetector gestureDetector;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,65 +19,30 @@ public class StartScreenActivity extends Activity {
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-		gestureDetector = new GestureDetector(this, new SwipeGestureDetector());
 		setContentView(R.layout.start_screen_activity);
 
-	}
+		Button newGame = (Button) findViewById(R.id.new_game);
+		Button quit = (Button) findViewById(R.id.quit);
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		if (gestureDetector.onTouchEvent(event)) {
-			return true;
-		}
-		return super.onTouchEvent(event);
-	}
+		newGame.setOnClickListener(new OnClickListener() {
 
-	private void onLeftSwipe() {
-		Intent intent = new Intent(StartScreenActivity.this,
-				LightsOutGameActivity.class);
-		startActivity(intent);
-		overridePendingTransition(R.animator.pull_in_from_right,
-				R.animator.push_out_to_left);
-	}
-
-	private void onRightSwipe() {
-		StartScreenActivity.this.finish();
-		overridePendingTransition(R.animator.pull_in_from_left,
-				R.animator.push_out_to_right);
-	}
-
-	// Private class for gestures
-	private class SwipeGestureDetector extends SimpleOnGestureListener {
-		// Swipe properties, you can change it to make the swipe
-		// longer or shorter and speed
-		private static final int SWIPE_MIN_DISTANCE = 120;
-		private static final int SWIPE_MAX_OFF_PATH = 200;
-		private static final int SWIPE_THRESHOLD_VELOCITY = 200;
-
-		@Override
-		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-				float velocityY) {
-			try {
-				float diffAbs = Math.abs(e1.getY() - e2.getY());
-				float diff = e1.getX() - e2.getX();
-
-				if (diffAbs > SWIPE_MAX_OFF_PATH)
-					return false;
-
-				// Left swipe
-				if (diff > SWIPE_MIN_DISTANCE
-						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-					StartScreenActivity.this.onLeftSwipe();
-
-					// Right swipe
-				} else if (-diff > SWIPE_MIN_DISTANCE
-						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-					StartScreenActivity.this.onRightSwipe();
-				}
-			} catch (Exception e) {
-				Log.e("YourActivity", "Error on gestures");
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(StartScreenActivity.this, LightsOutGameActivity.class);
+				startActivity(intent);
+				overridePendingTransition(R.animator.pull_in_from_right,
+						R.animator.push_out_to_left);
 			}
-			return false;
-		}
+		});
+
+		quit.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				StartScreenActivity.this.finish();
+				overridePendingTransition(R.animator.pull_in_from_left,
+						R.animator.push_out_to_right);
+			}
+		});
 	}
 }
